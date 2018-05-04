5f1da612b3e573aa70bd42cf71cc47011d29832f
Jake Lowen <jakelowen@gmail.com>
2018-05-04 11:38:22 -0500

    added special text when no contacts. Resolves #81

--- src/client/src/views/u/PotentialVotersList.jsx
+++ src/client/src/views/u/PotentialVotersList.jsx
@@ -75,10 +75,32 @@ class PotentialVotersList extends PureComponent {
                 <div className="content">
                   <h1 className="title">Get started by adding a contact.</h1>
                   <p>
-                    Danny will write verbose instructions to be placed here as per{' '}
-                    <a href="https://github.com/gpsimpact/relational_gotv/issues/42">
-                      github issue #42
-                    </a>
+                    Welcome to this non-partisan relational advocacy tool. Thank you for agreeing to
+                    reach out to your family, friends, and community to help engage them in voting.
+                    There is no more effective get out the vote effort than people getting the
+                    people they know to vote.
+                  </p>
+
+                  <p>
+                    This tool revolves around your contacts. When you add a contact, you will be
+                    presented with a list of tasks for that person. Some are pretty critical:
+                    registering them to vote, getting them to request an advance ballot, and getting
+                    them to vote. Others are engagement tasks along the way to those milestones. As
+                    you complete tasks, you will receive points, so you can see how you&apos;re
+                    doing.
+                  </p>
+
+                  <p>
+                    We encourage you to list at least ten people you know, and take the first step:
+                    matching them up to the public voter file. If you can&apos;t find a match, we
+                    have instructions for checking for errors or getting in touch to see if they are
+                    registered to vote, and registering them if they are not.
+                  </p>
+
+                  <p>
+                    In the end, you make all the difference. Your enthusiasm, your dedication to
+                    creating a culture of voting will convince your circle to get out and become
+                    engaged, too. Thank you.
                   </p>
                 </div>
               );
